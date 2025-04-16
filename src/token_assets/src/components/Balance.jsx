d7d6e06ddb46ec01e207393493cb7eabf7a7ade8
hard2hannana <hard2hannana>
2025-04-16 17:01:27 -0400

    balance correct

--- src/token_assets/src/components/Balance.jsx
+++ src/token_assets/src/components/Balance.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Principle } from '@dfinity/principle';
+import { token } from "../../../declarations/token"
 
 function Balance() {
   
+  const [inputValue, setInputVAlue] = useState("");
+  const [balanceResult, setBalance] = useState("")
+
   async function handleClick() {
-    console.log("Balance Button Clicked");
+    console.log(inputValue);
+    const principle = Principle.fromText(inputValue);
+    let balance = await token.balanceOf(principle);
+    setBalance(balance.toLocaleString);
   }
 
 
@@ -15,6 +23,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInputVAlue(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +35,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p>This account has a balance of {balanceResult}.</p>
     </div>
   );
 }
